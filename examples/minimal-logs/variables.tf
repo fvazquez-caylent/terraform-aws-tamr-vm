cad4562fb06d5b510d92185c8e9aa12f75a3945d
Federico Vazquez <federico.vazquez@caylent.com>
2021-12-09 16:11:17 -0300

    Adds key_name var.

--- examples/minimal-logs/variables.tf
+++ examples/minimal-logs/variables.tf
@@ -26,3 +26,9 @@ variable "egress_protocol" {
   type        = string
   description = "Protocol for egress rules. If not icmp, icmpv6, tcp, udp, or all use the protocol number."
 }
+
+
+variable "key_name" {
+  type        = string
+  description = "The key pair name."
+}
