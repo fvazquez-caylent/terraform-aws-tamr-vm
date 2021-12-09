1e0287376913f06a255a5a269d4a917c24306165
Federico Vazquez <federico.vazquez@caylent.com>
2021-12-09 18:19:35 -0300

    Removes unneded output.

--- examples/minimal-logs/outputs.tf
+++ examples/minimal-logs/outputs.tf
@@ -6,10 +6,10 @@ output "subnet_id" {
   value = aws_subnet.tamr_vm_subnet.id
 }
 
-output "tamr_private_key" {
-  value     = tls_private_key.tamr_ec2_private_key.private_key_pem
-  sensitive = true
-}
+#output "tamr_private_key" {
+#  value     = tls_private_key.tamr_ec2_private_key.private_key_pem
+#  sensitive = true
+#}
 
 output "tamr_vm" {
   value = module.tamr-vm
